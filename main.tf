e2f378433fbd89ab0994bb108afd1ca2b7cc3476
Karki <anup.karki@dbschenker.com>
2024-09-25 13:12:02 +0200

    checking push stage

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0" # Specify the exact version
+    }
+  }
+}
+
+//adding comment to check Push
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Set the region in the provider block
 }
 
 module "s3_bucket" {
